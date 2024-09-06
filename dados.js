f6ea45ba85a7c6f749f387fb4f51feb5e27faba1
João Marcos <131201827+joaom09@users.noreply.github.com>
2024-09-06 10:19:15 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -9,11 +9,11 @@ let dados = [
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        tags: "judô judo judoka"
+        tags: "ginástica ginastica ouro"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua força e técnica a colocaram entre as melhores do mundo",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        tags: "ginástica ginastica ouro"
+        tags: "judô judo judoka"
     }]
