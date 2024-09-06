ef2d3275a5b759b86146947cab49d3c8cd17658c
João Marcos <131201827+joaom09@users.noreply.github.com>
2024-09-06 10:05:52 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua força e técnica a colocaram entre as melhores do mundo",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginástica ginastica ouro"
+    }]
